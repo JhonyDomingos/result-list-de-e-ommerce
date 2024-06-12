105c7d3ef32fc5fc000c1239187e2b6020ead0b4
JhonyDomingos <jhonatandomingossilva@oulook.com.br>
2024-06-12 02:42:52 -0300

    feat(app): render the header component in the app

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,7 @@
+
+import { Header } from "./components/header/Header";
 import "./styles/index.css";
 
-function App() {
-  return (
-    <>
-      <h1>Hello world</h1>
-      <h2>Again !!</h2>
-    </>
-  );
-}
-
-export default App;
+export const App = () => {
+  return <Header name="Compass"/>;
+};
