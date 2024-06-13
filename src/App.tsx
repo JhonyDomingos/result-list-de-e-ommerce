6364f65cc24ae1ad112abf6ca8ad777b7b309cfd
JhonyDomingos <jhonatandomingossilva@oulook.com.br>
2024-06-13 00:57:46 -0300

    feat: added new ShopSection component in app.tsx
    Implemented a new ShopSection component in app.tsx for rendering.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
-
 import { Header } from "./components/header/Header";
+import { ShopSection } from "./components/ShopSection/ShopSection";
 import "./styles/index.css";
 
 export const App = () => {
-  return <Header name="Compass"/>;
+  return (
+    <>
+      <Header name="Compass" />
+      <ShopSection />
+    </>
+  );
 };
