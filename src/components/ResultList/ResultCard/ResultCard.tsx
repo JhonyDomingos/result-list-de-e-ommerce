4a3ab7b92838d1f31ef22afc5d2c375f766e1853
JhonyDomingos <jhonatandomingossilva@oulook.com.br>
2024-06-16 09:56:54 -0300

    feat(style): finishing the overflow on cardSection

--- src/components/ResultList/ResultCard/ResultCard.tsx
+++ src/components/ResultList/ResultCard/ResultCard.tsx
@@ -1,12 +1,43 @@
 import { IProduct } from "../../../database/productDatabase";
+import compareIcon from "../../../assets/icons/compare-svgrepo-com.svg";
+import like from "../../../assets/icons/Heart.svg";
+import share from "../../../assets/icons/share.svg";
 import cardStyles from "./resultCard.module.css";
+import { Button } from "../../Buttons/defaultButtons/buttons";
 interface IReusltCardProps {
   product: IProduct;
+  
 }
 
 export const ResultCard = ({ product }: IReusltCardProps) => {
+  
   return (
-    <li className={cardStyles.cardList}>
+    <li className={cardStyles.cardList} >
+      <div className={cardStyles.overlay}>
+        <div className={cardStyles.overlayButton}>
+          <Button>See Details</Button>
+        </div>
+        <div className={cardStyles.overlayItens}>
+          <div className={cardStyles.items}>
+            <a href="#">
+              <img src={share} alt="" />
+              Share
+            </a>
+          </div>
+          <div className={cardStyles.items}>
+            <a href="#">
+              <img src={compareIcon} alt="" />
+              Compare
+            </a>
+          </div>
+          <div className={cardStyles.items}>
+            <a href="#">
+              <img src={like} alt="" />
+              Like
+            </a>
+          </div>
+        </div>
+      </div>
       <figure>
         <img src={product.image} alt={product.name} />
       </figure>
@@ -15,7 +46,9 @@ export const ResultCard = ({ product }: IReusltCardProps) => {
       <div className={cardStyles.price}>
         {product.discountedPrice && product.discountedPrice < product.price ? (
           <>
-            <p className={cardStyles.discontedPrice}>Rp {product.discountedPrice}</p>
+            <p className={cardStyles.discontedPrice}>
+              Rp {product.discountedPrice}
+            </p>
             <p className={cardStyles.originalPrice}>Rp {product.price}</p>
           </>
         ) : (
